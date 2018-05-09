a6ef59d6da934141e5871c462939c280e99c9f44
Runner <841766635@qq.com>
2018-05-09 23:27:58 +0800

    更新答案

---
+++ Algorithms/167两数之和 II - 输入有序数组two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,12 @@
+/** * @param {number[]} numbers * @param {number} target * @return {number[]} */
+var twoSum = function(numbers, target) {
+    for (var i = 0; i < numbers.length; i++) {
+        for (var j = i + 1; j < numbers.length; j++) {
+            var num1 = numbers[i];
+            var num2 = numbers[j];
+            if (num1 + num2 == target) {
+                return [i + 1, j + 1];
+            }
+        }
+    }
+};
