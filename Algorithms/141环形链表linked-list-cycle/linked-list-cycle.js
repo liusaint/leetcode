620034c153444c6a7f63e59af6eda49759c16200
Runner <841766635@qq.com>
2018-05-19 23:08:01 +0800

    更新

---
+++ Algorithms/141环形链表linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,22 @@
+/** * Definition for singly-linked list. * function ListNode(val) { *     this.val = val; *     this.next = null; * } */ /** * @param {ListNode} head * @return {boolean} */ /*环并不一定是首尾连接*/
+var hasCycle = function(head) {
+    if (head == null) {
+        return false;
+    }
+    if (head.next == null) {
+        return false;
+    }
+    if (head.next.next == null) {
+        return false;
+    }
+    var slow = head;
+    var fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false;
+};
