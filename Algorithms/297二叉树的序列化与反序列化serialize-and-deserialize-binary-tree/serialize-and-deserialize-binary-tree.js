abf7f178c4f03a8e983525e0e73103d76614f129
Runner <841766635@qq.com>
2018-05-28 23:08:23 +0800

    更新答案

---
+++ Algorithms/297二叉树的序列化与反序列化serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.js
@@ -0,0 +1,59 @@
+/*只要有一个节点不为空，那么一定要有它的左右节点。*/
+var serialize = function(root) {
+    if (root == null) {
+        return '[]';
+    }
+    var checkArr = [root];
+    var resArr = [root.val];
+    while (checkArr.length > 0) {
+        var newCheckArr = [];
+        for (var i = 0; i < checkArr.length; i++) {
+            var item = checkArr[i];
+            if (item.left) {
+                newCheckArr.push(item.left);
+                resArr.push(item.left.val);
+            } else {
+                resArr.push(null);
+            }
+            if (item.right) {
+                newCheckArr.push(item.right);
+                resArr.push(item.right.val);
+            } else {
+                resArr.push(null);
+            }
+        }
+        checkArr = newCheckArr;
+    }
+    return JSON.stringify(resArr);
+}; /** * Decodes your encoded data to tree. * * @param {string} data * @return {TreeNode} */
+var deserialize = function(data) {
+    if (data == '[]') {
+        return null;
+    }
+    var arr = JSON.parse(data);
+    var len = arr.length;
+    var root = new TreeNode(arr.shift());
+    var nextArr = [root];
+    while (arr.length > 0) {
+        var newCheckArr = [];
+        for (var i = 0; i < nextArr.length; i++) {
+            var item = nextArr[i];
+            var arrItem1 = arr.shift();
+            if (arrItem1 == 'null' || arrItem1 == null) {
+                item.left = null;
+            } else {
+                item.left = new TreeNode(arrItem1);
+                newCheckArr.push(item.left);
+            }
+            var arrItem2 = arr.shift();
+            if (arrItem2 == 'null' || arrItem2 == null) {
+                item.right = null;
+            } else {
+                item.right = new TreeNode(arrItem2);
+                newCheckArr.push(item.right);
+            }
+        }
+        nextArr = newCheckArr;
+    }
+    return root;
+}; /** * Your functions will be called as such: * deserialize(serialize(root)); */
---
+++ Algorithms/662二叉树最大宽度maximum-width-of-binary-tree/maximum-width-of-binary-tree.js
@@ -0,0 +1,29 @@
+/*层次遍历，每个节点加一个index。根据上层index和left,right来计算下层的index。*/
+var widthOfBinaryTree = function(root) {
+    if (root == null) {
+        return 0;
+    }
+    var checkArr = [root];
+    root.index = 1;
+    var maxLen = 1;
+    while (checkArr.length > 0) {
+        var newCheckArr = [];
+        for (var i = 0; i < checkArr.length; i++) {
+            var item = checkArr[i];
+            if (item.left) {
+                newCheckArr.push(item.left);
+                item.left.index = item.index * 2 - 1;
+            }
+            if (item.right) {
+                newCheckArr.push(item.right);
+                item.right.index = item.index * 2;
+            }
+        }
+        var len = checkArr[checkArr.length - 1].index - checkArr[0].index + 1;
+        if (len > maxLen) {
+            maxLen = len;
+        }
+        checkArr = newCheckArr;
+    }
+    return maxLen;
+};
